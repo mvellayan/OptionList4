d03d6c7a3d211a4b925f569ccf7657648cd7725e
mvellayan <muthu.vellayan@gmail.com>
2021-11-13 10:04:30 -0500

    made a standalone call for building optionlist

---
+++ pull_option_list.py
@@ -0,0 +1,37 @@
+from pprint import pprint
+import sys
+
+from ib_insync import *
+
+from utils import FileUtil, IBUtil
+
+#
+# see: https://github.com/erdewit/ib_insync
+config = {}
+ib = IB()
+
+def main(configFileName):
+    global config
+    config = FileUtil.readConfig(configFileName)
+
+    print("Connecting to ip [", config["tws_host"], "] port[", config["tws_port"], "] clientId [", config["tws_port"], "]")
+    try:
+        ib.connect(config["tws_host"], config["tws_port"], clientId=config["tws_port"])
+    except BaseException as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        raise NameError("Cannot connect to IB")
+
+    print("Connection Status: ", ib.isConnected())
+
+    IBUtil.writeContractFile(ib, config)
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("\n\nUsage: collect_data.py <config_file.yml>\n\n")
+        sys.exit(0)
+    else:
+        print("using config file [" + sys.argv[1] + "]")
+
+    main(sys.argv[1])
