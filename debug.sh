d43947b7b767d8435143c702f3152ece7cc28afc
Muthu Vellayan <muthu.vellayan@gmail.com>
2021-11-17 16:05:39 -0500

    ...

--- debug.sh
+++ debug.sh
@@ -1,1 +1,1 @@
-docker container run --network host -it -v $(pwd)/IBdata:/IBapp/IBdata ol4 python3 realtime_data_collector.py config/config-aapl.jso
+docker container run --network host -it -v $(pwd)/IBdata:/IBapp/IBdata ol4 python3 realtime_data_collector.py config/config-aapl.json
