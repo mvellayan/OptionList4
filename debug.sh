42f6dd8d26abfacfca1b2b150ca8832ca35987cb
mvellayan <muthu.vellayan@gmail.com>
2021-12-06 08:33:20 -0500

    refactored / renamed file name for consistency

--- debug.sh
+++ debug.sh
@@ -1,1 +1,1 @@
-docker container run --network host -it -v $(pwd)/IBdata:/IBapp/IBdata ol4 python3 realtime_data_collector.py config/config-aapl.json
+docker container run --network host -it -v $(pwd)/IBdata:/IBapp/IBdata ol4 python3 ib_realtime_18.py config/config-aapl.json
--- project.sh
+++ project.sh
@@ -1,2 +1,2 @@
-docker container run -d  -v $(pwd)/IBdata:/IBapp/IBdata ol4 python3 model_project_options.py config/config-aapl.json
-docker container run -d  -v $(pwd)/IBdata:/IBapp/IBdata ol4 python3 model_project_options.py config/config-fb.json
+docker container run -d  -v $(pwd)/IBdata:/IBapp/IBdata ol4 python3 ib_realtime_18.py config/config-aapl.json
+docker container run -d  -v $(pwd)/IBdata:/IBapp/IBdata ol4 python3 ib_realtime_18.py config/config-fb.json
