36d9b2c81f37eb019d32cc22ead5603d99d1f82a
Muthu Vellayan <muthu.vellayan@gmail.com>
2021-12-19 10:19:12 -0500

    adding jupyter notebook capability in dockerized server

--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
 #docker container run --network host -it -v $(pwd)/IBdata:/IBapp/IBdata -v $(pwd)/logs:/logs -v  /home/mvellayan/.aws/:/opt/app-root/src/.aws ol4 python3 ib_realtime_18.py config/config-aapl.json
-docker container run --network host -it -v $(pwd)/IBdata:/IBapp/IBdata -v $(pwd)/logs:/logs -v  /home/mvellayan/.aws/:/opt/app-root/src/.aws ol4 /bin/bash
+docker container run --network host -it -v $(pwd)/IBdata:/IBapp/IBdata -v $(pwd)/logs:/logs -v $(pwd)/ml_cp18:/IBapp/ml_cp18 -v  /home/mvellayan/.aws/:/opt/app-root/src/.aws ol4 /bin/bash
