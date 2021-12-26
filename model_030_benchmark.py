a6cdbb9fc9a4c3fe019d253b7e05e023cbae3d7f
mvellayan <muthu.vellayan@gmail.com>
2021-12-26 13:42:02 -0500

    working .

--- model_030_benchmark.py
+++ model_030_benchmark.py
@@ -21,17 +21,21 @@ from utils.FileUtil import makeDirectory, unzip_file, get_sec_to_expire, getDate
 '''
 Outline of functionality:
 # 1. Validate parameters
-# 2. Print Stat 1: Confusion Matrix Expected vs Actual
-# 3. Build hashmap of test-file by time, for search performance reasons
-# 4. Simulate trade & build data model
-# 5. Report Summary / Detail
-
+#       load model
+#       load test data file
+# 2. Build hashmap of test-file by time, for search performance reasons
+# 3. Simulate trade & build data model
+# 5. Print Stat 1: Confusion Matrix Expected vs Actual
+# 6. Report Summary / Detail
 '''
 
 
 logging.basicConfig(level=logging.ERROR)
 log = logging.getLogger("myLogger")
 log.setLevel(logging.INFO)
+pd.set_option('display.max_rows', 15)
+pd.set_option('display.width', 150)
+pd.set_option('display.max_columns', 10)
 
 
 def plot_confusion_matrix(cm, classes, normalized=True, cmap='bone', fmt='g'):
@@ -45,12 +49,7 @@ def plot_confusion_matrix(cm, classes, normalized=True, cmap='bone', fmt='g'):
 def expandX(p0, p1, p2, p3, p4, p5, p6, labels):
     arr1 = [p0,  p1, p2, p3, p4, p5, p6]
     label = find_label(arr1, labels)
-    retDict = {}
-    ctr = 0
-    for label in labels:
-        retDict[label] = arr1[ctr]
-        ctr += 1
-    retDict['prediction'] = label
+    retDict = {'prediction': label}
     return retDict
 
 
@@ -71,14 +70,19 @@ def find_label(prob, labels):
 
 def main(test_file, model_file):
 
-    needed_cols = ['bid_size', 'ask_size', 'last_size', 'volume',  'bid_ask_delta',  'c_w1_n3_ask', 'c_w1_n3_bid', 'c_w1_n3_last', 'c_w1_n3_last_size',  'c_w1_n3_time_value',  'c_w1_n3_theta',  'c_w1_n2_ask',  'c_w1_n2_bid',  'c_w1_n2_last',  'c_w1_n2_last_size',  'c_w1_n2_time_value',  'c_w1_n2_theta',  'c_w1_n1_ask',  'c_w1_n1_bid',  'c_w1_n1_last',  'c_w1_n1_last_size',  'c_w1_n1_time_value',  'c_w1_n1_theta',  'c_w1_p1_ask',  'c_w1_p1_bid',  'c_w1_p1_last',  'c_w1_p1_last_size',  'c_w1_p1_time_value',  'c_w1_p1_theta',  'c_w1_p2_ask',  'c_w1_p2_bid',  'c_w1_p2_last',  'c_w1_p2_last_size',  'c_w1_p2_time_value',  'c_w1_p2_theta',  'c_w1_p3_ask',  'c_w1_p3_bid',  'c_w1_p3_last',  'c_w1_p3_last_size',  'c_w1_p3_time_value',  'c_w1_p3_theta',  'c_w2_n3_ask',  'c_w2_n3_bid',  'c_w2_n3_last',  'c_w2_n3_last_size',  'c_w2_n3_time_value',  'c_w2_n3_theta',  'c_w2_n2_ask',  'c_w2_n2_bid',  'c_w2_n2_last',  'c_w2_n2_last_size',  'c_w2_n2_time_value',  'c_w2_n2_theta',  'c_w2_n1_ask',  'c_w2_n1_bid',  'c_w2_n1_last',  'c_w2_n1_last_size',  'c_w2_n1_time_value',  'c_w2_n1_theta',  'c_w2_p1_ask',  'c_w2_p1_bid',  'c_w2_p1_last',  'c_w2_p1_last_size',  'c_w2_p1_time_value',  'c_w2_p1_theta',  'c_w2_p2_ask',  'c_w2_p2_bid',  'c_w2_p2_last',  'c_w2_p2_last_size',  'c_w2_p2_time_value',  'c_w2_p2_theta',  'c_w2_p3_ask',  'c_w2_p3_bid',  'c_w2_p3_last',  'c_w2_p3_last_size',  'c_w2_p3_time_value',  'c_w2_p3_theta',  'c_w3_n3_ask',  'c_w3_n3_bid',  'c_w3_n3_last',  'c_w3_n3_last_size',  'c_w3_n3_time_value',  'c_w3_n3_theta',  'c_w3_n2_ask',  'c_w3_n2_bid',  'c_w3_n2_last',  'c_w3_n2_last_size',  'c_w3_n2_time_value',  'c_w3_n2_theta',  'c_w3_n1_ask',  'c_w3_n1_bid',  'c_w3_n1_last',  'c_w3_n1_last_size',  'c_w3_n1_time_value',  'c_w3_n1_theta',  'c_w3_p1_ask',  'c_w3_p1_bid',  'c_w3_p1_last',  'c_w3_p1_last_size',  'c_w3_p1_time_value',  'c_w3_p1_theta',  'c_w3_p2_ask',  'c_w3_p2_bid',  'c_w3_p2_last',  'c_w3_p2_last_size',  'c_w3_p2_time_value',  'c_w3_p2_theta',  'c_w3_p3_ask',  'c_w3_p3_bid',  'c_w3_p3_last',  'c_w3_p3_last_size',  'c_w3_p3_time_value', 'c_w3_p3_theta']
-
+    needed_cols = ['bid_size', 'ask_size', 'last_size', 'volume',  'bid_ask_delta',  'c_w1_n3_ask', 'c_w1_n3_bid', 'c_w1_n3_last', 'c_w1_n3_last_size',  'c_w1_n3_time_value',  'c_w1_n3_theta',  'c_w1_n2_ask',  'c_w1_n2_bid',  'c_w1_n2_last',  'c_w1_n2_last_size',  'c_w1_n2_time_value',  'c_w1_n2_theta',  'c_w1_n1_ask',  'c_w1_n1_bid',  'c_w1_n1_last',  'c_w1_n1_last_size',  'c_w1_n1_time_value',  'c_w1_n1_theta',  'c_w1_p1_ask',  'c_w1_p1_bid',  'c_w1_p1_last',  'c_w1_p1_last_size',  'c_w1_p1_time_value',  'c_w1_p1_theta',  'c_w1_p2_ask',  'c_w1_p2_bid',  'c_w1_p2_last',  'c_w1_p2_last_size',  'c_w1_p2_time_value',  'c_w1_p2_theta',  'c_w1_p3_ask',  'c_w1_p3_bid',  'c_w1_p3_last',  'c_w1_p3_last_size',  'c_w1_p3_time_value',  'c_w1_p3_theta',  'c_w2_n3_ask',  'c_w2_n3_bid',  'c_w2_n3_last',  'c_w2_n3_last_size',  'c_w2_n3_time_value',  'c_w2_n3_theta',  'c_w2_n2_ask',  'c_w2_n2_bid',  'c_w2_n2_last',  'c_w2_n2_last_size',  'c_w2_n2_time_value',  'c_w2_n2_theta',  'c_w2_n1_ask',  'c_w2_n1_bid',  'c_w2_n1_last',  'c_w2_n1_last_size',  'c_w2_n1_time_value',  'c_w2_n1_theta',  'c_w2_p1_ask',  'c_w2_p1_bid',  'c_w2_p1_last',  'c_w2_p1_last_size',  'c_w2_p1_time_value',  'c_w2_p1_theta',  'c_w2_p2_ask',  'c_w2_p2_bid',  'c_w2_p2_last',  'c_w2_p2_last_size',  'c_w2_p2_time_value',  'c_w2_p2_theta',  'c_w2_p3_ask',  'c_w2_p3_bid',  'c_w2_p3_last',  'c_w2_p3_last_size',  'c_w2_p3_time_value',  'c_w2_p3_theta',  'c_w3_n3_ask',  'c_w3_n3_bid',  'c_w3_n3_last',  'c_w3_n3_last_size',  'c_w3_n3_time_value',  'c_w3_n3_theta',  'c_w3_n2_ask',  'c_w3_n2_bid',  'c_w3_n2_last',  'c_w3_n2_last_size',  'c_w3_n2_time_value',  'c_w3_n2_theta',  'c_w3_n1_ask',  'c_w3_n1_bid',  'c_w3_n1_last',  'c_w3_n1_last_size',  'c_w3_n1_time_value',  'c_w3_n1_theta',  'c_w3_p1_ask',  'c_w3_p1_bid',  'c_w3_p1_last',  'c_w3_p1_last_size',  'c_w3_p1_time_value',  'c_w3_p1_theta',  'c_w3_p2_ask',  'c_w3_p2_bid',  'c_w3_p2_last',  'c_w3_p2_last_size',  'c_w3_p2_time_value',  'c_w3_p2_theta',  'c_w3_p3_ask',  'c_w3_p3_bid',  'c_w3_p3_last',  'c_w3_p3_last_size',  'c_w3_p3_time_value', 'c_w3_p3_theta', 'p300s_bucket_category']
 
     # 1.0 load data, model
     data = pd.read_csv(test_file)
     try:
-        data_X = data[needed_cols]
-    except KeyError:
+        have_cols = [value for value in needed_cols if value in list(data.columns)]
+        X = data [have_cols]
+        X.dropna(axis=0, inplace=True)
+        Y = X['p300s_bucket_category'].to_frame()
+        X.drop(['p300s_bucket_category'], axis=1, inplace=True)
+
+    except KeyError as e:
+        print (e)
         have_cols = list(data.columns)
         print(f"can find required values in file {test_file}")
         print(f"missing cols: {set(needed_cols) - set(have_cols)}")
@@ -88,45 +92,54 @@ def main(test_file, model_file):
 
     # 2.0 cleanup ?
     #curPd = curPd[(curPd['time'] % 1000000).between(94500, 154500)]
-    data_X = data.dropna(axis=1)
-    data_X.assign(idx=lambda x: x.index)
+    # data_X.assign(idx=lambda x: x.index)
 
     # 3. Run Inference Here
     ##
     categories = data.p300s_bucket_category.unique().tolist()
+    categories = categories[:-1]
+
     xgb_model = xgb.Booster()
     xgb_model.load_model(model_file)
-    data_X_dmatrix = xgb.DMatrix(data_X)
-    y_pred = xgb_model.predict(data_X_dmatrix)
+    model_features = xgb_model.feature_names
+    print(f"missing cols in model: {set(model_features) - set(X.columns)}")
+
+    X_dmatrix = xgb.DMatrix(X)
+    y_pred = xgb_model.predict(X_dmatrix)
     y_pred_df = pd.DataFrame(y_pred, columns=categories)
 
-    expanded_df = pd.concat([data_X, y_pred_df], axis=1)
+    y_pred_df['prediction'] = y_pred_df.apply(lambda x:
+                                expandX(x[0], x[1], x[2], x[3], x[4], x[5], x[6], categories),
+                                axis=1, result_type='expand')
+    y_pred_df['prediction_3'] = y_pred_df['prediction'].str[0:3]
+    aaa = y_pred_df["prediction_3"].value_counts()
+
+    # expanded_df = pd.concat([X, y_pred_df], axis=1)
     # expanded_df = pd.bind ### similar to concat
 
-    print(expanded_df)
-
-    print(y_pred_df)
-
-    y_pred_df = y_pred_df.apply(lambda x: expandX(x[0], x[1], x[2], x[3], x[4], x[5], x[6], categories),
-                                axis=1, result_type='expand')
-    print(y_pred_df)
+    Y['p300s_bucket_category_3'] = Y['p300s_bucket_category'].str[0:3]
 
     # cm = confusion_matrix(data['p300s_bucket_category'], y_pred_df['prediction'])
     # plot_confusion_matrix(cm, classes=categories, cmap='YlGnBu')
     # 4. print confusion matrix
-    cm = confusion_matrix(data['p300s_bucket_category'], data['p300s_bucket_category'])
-    cm_pct = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    print(cm)
-    print(cm_pct)
 
+    bbb = Y["p300s_bucket_category_3"].value_counts()
+    cm = confusion_matrix(y_pred_df['prediction_3'], Y['p300s_bucket_category_3'])
+    cm_pd = pd.DataFrame(cm, index=categories, columns=categories)
+
+    cm_pct = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    cm_pct_pd = pd.DataFrame(cm_pct, index=categories, columns=categories)
+
+    print(cm_pd)
+    print(cm_pct_pd)
+
+    return
     # 5. Load pdOptionQuotesIdx -- Options Quotes
     pdOptionQuotesIdx = {}
     for index, row in curPd.iterrows():
         hash_idx = str(row.time)
         # print (index, '->', row, '==>', hash_idx)
         pdOptionQuotesIdx[hash_idx] = row
-
-
 
 
     #  3. loop through with trade runs
