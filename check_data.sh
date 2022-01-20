ba9beec7e1ab02e3b9a8f1ff08ad84d658d43fe8
Muthu Vellayan <muthu.vellayan@gmail.com>
2022-01-20 15:49:05 -0500

    checking for appl and fb seperately

--- check_data.sh
+++ check_data.sh
@@ -1,6 +1,14 @@
-if [ $(find ~/Development/OptionList4/IBdata/2022/ -type f -mmin -3 | wc -l) -gt 0 ]
+if [ $(find ~/Development/OptionList4/IBdata/2022/ -type f -mmin -3 | grep AAPL | wc -l) -gt 0 ]
 then
-    echo 'found new data'
+    echo 'found new AAPL data'
 else
-    aws sns publish --topic-arn "arn:aws:sns:us-east-1:775579389744:notifyMuthu" --subject "No New Data" --message "no new data"
+    /usr/local/bin/aws sns publish --topic-arn "arn:aws:sns:us-east-1:775579389744:notifyMuthu" --subject "No New AAPL Data" --message "no new AAPL data"
+fi
+#
+#
+if [ $(find ~/Development/OptionList4/IBdata/2022/ -type f -mmin -3 | grep FB | wc -l) -gt 0 ]
+then
+    echo 'found new FB data'
+else
+    /usr/local/bin/aws sns publish --topic-arn "arn:aws:sns:us-east-1:775579389744:notifyMuthu" --subject "No New FB Data" --message "no new FB data"
 fi
