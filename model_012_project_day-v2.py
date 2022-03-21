0f7e0d38d0c69babbad2f3c6fce6bcc19f6637e5
mvellayan <muthu.vellayan@gmail.com>
2022-03-21 10:15:55 -0400

    Working checkpoint.  about to refactor bigtime

--- model_012_project_day-v2.py
+++ model_012_project_day-v2.py
@@ -285,15 +285,17 @@ def main(model_no, in_zip_files, out_dir, symbol: str):
     print(f"max seen ctr = {ctr}")
     print("last stock", open_stock)
     flush_row(rows, outfile)
-    print("\n\n############################################################################")
     print("final results\n")
     res = pd.DataFrame(rows, columns=fields)
     res["net_per_day"] = res["net"] / res["dur-days"]
 
+    ######################################################################################################################
     # reference
     # https://xlsxwriter.readthedocs.io/working_with_pandas.html
     statuses = ['sold', 'expired', 'not sold1', 'no data']
     summary_outfile = outfile.replace(".csv", '_summary.xlsx')
+    row_ct = 1
+    writer = pd.ExcelWriter(summary_outfile, engine='xlsxwriter')
     for status in statuses:
         summaryPD = res.loc[res['sold'] == status]
         summaryStat = summaryPD.groupby('net').agg({
@@ -311,18 +313,33 @@ def main(model_no, in_zip_files, out_dir, symbol: str):
             'net_per_day': ['mean'],
             'net': ['mean', 'sum']
             })
-        ## remove zeros in net, meybe
-        summaryStat.index.name = status
-        summaryStat.to_excel(summary_outfile)
+
+        if summaryStat.shape[0] > 0:
+            ## remove zeros in net, meybe
+            summaryStat.index.name = status
+            summaryStat.to_excel(writer, sheet_name='SheetA', startrow=row_ct)
+            row_ct += summaryStat.shape[0] + 1
+
+    # Get the xlsxwriter workbook and worksheet objects.
+    workbook = writer.book
+    worksheet = writer.sheets['SheetA']
+    format2 = workbook.add_format({'num_format': '#,##0.00'})
+    format0 = workbook.add_format({'num_format': '#'})
+    worksheet.set_column(2, 13, None, format2)
+    worksheet.conditional_format(0, 13, row_ct + 2, 13,
+                                 {'type': '3_color_scale'})
+
+    writer.save()
 
 
+######################################################################################################################
     fields = ["sold", "o_date", "o_time", "o_stock_ask", "o_option_bid", "strike", "expiry", "o_tv", "o_iv",
               "o_theta", "o_dr", "c_date", "c_time", "c_stock_bid", "c_option_ask", "c_tv", "c_iv", "c_theta",
               "c_dr", "net_option", "net_stock", "net", "dur-days"]
     # reference
     # https://xlsxwriter.readthedocs.io/working_with_pandas.html
     summary_outfile = outfile.replace(".csv", '_summary_by_day.xlsx')
-    summaryStat = res.groupby(['o_date', 'sold', 'expiry']).agg({
+    summaryStat = res.groupby([ 'sold', 'o_date', 'expiry']).agg({
         'o_stock_ask': ['count', 'mean', 'min', 'max'],
         'o_theta': ['mean'],
         'o_tv': ['mean'],
@@ -340,7 +357,21 @@ def main(model_no, in_zip_files, out_dir, symbol: str):
         'net': ['mean', 'sum']
         })
     # summaryStat.index.name = status
-    summaryStat.to_excel(summary_outfile)
+    writer = pd.ExcelWriter(summary_outfile, engine='xlsxwriter')
+    summaryStat.to_excel(writer, sheet_name='SheetA')
+
+    # Get the xlsxwriter workbook and worksheet objects.
+    workbook = writer.book
+    worksheet = writer.sheets['SheetA']
+    # Add some cell formats.
+    format2 = workbook.add_format({'num_format': '#,##0.00'})
+    format0 = workbook.add_format({'num_format': '#'})
+    worksheet.set_column(4, 22, None, format2)
+    worksheet.conditional_format(0, 21, summaryStat.shape[0] + 2, 21,
+                                 {'type': '3_color_scale'})
+    writer.save()
+
+    # summaryStat.to_excel(summary_outfile)
 
 
 
@@ -373,7 +404,7 @@ if __name__ == "__main__":
         print (index, zip_file)
         if zip_file[-16:] in ['AAPL20220111.zip']:
             startTime = time.time()
-            main(model_no,  file_list[index: index+2], out_dir_m, symbol_m)
+            main(model_no,  file_list[index: index+7], out_dir_m, symbol_m)
             print(f'TIME Main: {(time.time() - startTime):.2f}')
             break
 
